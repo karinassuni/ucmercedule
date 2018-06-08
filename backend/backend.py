84563bec57074aebc3cadc83d4f232975e6c4db4
Karina Antonio <karinafantonio@gmail.com>
2018-06-08 09:04:40 -0700

    (Correctly) create department_fieldset with checkboxes in Mustache

--- backend/backend.py
+++ backend/backend.py
@@ -1,37 +1,17 @@
-import json
 import pystache
 import ucmscraper
 
-def createJSConstants(schedule):
-    def writeJSConstantFile(obj, constantName):
-        contents = 'var {} = {}'.format(constantName, json.dumps(obj))
-        with open('{}.js'.format(constantName), 'w') as f:
-            f.write(contents)
-    
-    departmentMenu = {
-        code: {
-            "name": name,
-            # We iterate through sections but we extract course-level info
-            "courses": {
-                # Because we're using a map, and because all of a course's
-                # sections have the same courseNumber, there are no duplicates
-                section['courseNumber']: section['title']
-                for section in schedule.sections
-            }
-        }
-        for code, name in schedule.departments.items()
-    }
-    writeJSConstantFile(departmentMenu, 'departmentMenu')
-
-    def getID(course):
-        return '{}-{}-{}'.format(course["departmentCode"], course["courseNumber"], course["section"])
-
-    sectionIndex = {
-        getID(section): section
-        for section in schedule.sections
-    }
-    writeJSConstantFile(sectionIndex, 'sectionIndex')
-
 validterms = ucmscraper.fetchValidterms()
 schedule = ucmscraper.Schedule(validterms[-1]) # latest term
-createJSConstants(schedule)
+
+departments_view = {
+    'departments': [
+        {'code': code, 'name': name}
+        for code, name in schedule.departments.items()
+    ]
+}
+with open('department_fieldset.mustache', 'r') as f:
+    department_fieldset_template = f.read()
+with open('department_fieldset.html', 'w') as f:
+    templated = pystache.render(department_fieldset_template, departments_view)
+    f.write(templated)
