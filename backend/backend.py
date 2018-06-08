180330cdf9a6fb8158fe407640c016a247c3ee9b
Karina Antonio <karinafantonio@gmail.com>
2018-06-08 16:24:04 -0700

    Upgrade ucmscraper and check for existing departments_view.json

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,22 @@
+import json
 import pystache
 import ucmscraper
 
-validterms = ucmscraper.fetchValidterms()
-schedule = ucmscraper.Schedule(validterms[-1]) # latest term
+DEPARTMENTS_VIEW_FILENAME = 'departments_view.json'
+try:
+    with open(DEPARTMENTS_VIEW_FILENAME, 'r') as f:
+        departments_view = json.load(f)
+except FileNotFoundError:
+    schedule = ucmscraper.Schedule.fetch_latest()
+    departments_view = {
+        'departments': [
+            {'code': code, 'name': name}
+            for code, name in schedule.departments.items()
+        ]
+    }
+    with open(DEPARTMENTS_VIEW_FILENAME, 'w') as f:
+        json.dump(departments_view, f)
 
-departments_view = {
-    'departments': [
-        {'code': code, 'name': name}
-        for code, name in schedule.departments.items()
-    ]
-}
 with open('department_fieldset.mustache', 'r') as f:
     department_fieldset_template = f.read()
 with open('department_fieldset.html', 'w') as f:
