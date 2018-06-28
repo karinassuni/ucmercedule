03a83ff62ab008f0776b778d87b39f7cf1304315
Karina Antonio <karinafantonio@gmail.com>
2018-06-27 19:29:02 -0700

    Arrange courses and their sections into proper hierarchy and update ucmscraper

--- backend/sections_view.py
+++ backend/sections_view.py
@@ -1,16 +1,25 @@
 import itertools
-from more_itertools import peekable
 import json
 import ucmscraper
 
 def course_id(section):
-    # different activity types (lecture, discussion, labs) have the same ID
+    # NOTE that labs that are their own courses (with units, lecture, and lab
+    # section) are totally a thing;
+    # e.g. BIO-001L with -01, -02, etc. lab sections, separate from BIO-001
+
+    # IF CERTAIN SECTION TYPES ARE *REQUIRED* TO BE TAKEN TOGETHER, ALL THOSE
+    # SECTIONS/TYPES WILL HAVE THE SAME COURSE ID
+    # e.g. CHEM-002 requires lab and discussion; lect, lab, and discussion
+    # sections are all CHEM-002 but with -02D, -02DL, -03D, -03DL sections
+    #                                    ^^^^^^^^^^^ lab & disc. must match here
+    # that are unitless, and lecture -01, -20 lecture sections that have units
     return section['departmentCode'] + ' ' + section['courseNumber']
 
 def is_sample(section):
     return (
-        course_id(section) == 'CSE 005' or
+        course_id(section) == 'CHEM 002' or
         course_id(section) == 'BIO 001' or
+        course_id(section) == 'BIO 001L' or
         course_id(section) == 'SOC 001' or
         course_id(section) == 'WRI 010'
     )
@@ -29,35 +38,49 @@ def main():
     schedule = ucmscraper.Schedule.fetch_latest()
     sample_sections = filter(is_sample, schedule.sections)
 
-    sections_by_course = {'courses': []}
+    courses = []
     for _, course_sections in itertools.groupby(sample_sections, key=course_id):
-        course = peekable(course_sections)
-        sections_by_course['courses'].append(
-            select_fields(course.peek(),
-                'departmentCode',
-                'courseNumber',
-                'title',
-                'notes',
-                'units',
-                ('sections', [
-                    select_fields(section,
-                        'CRN',
-                        'section',
-                        'days',
-                        'startTime',
-                        'endTime',
-                        'instructor',
-                        'maxSeats',
-                        'takenSeats',
-                        'freeSeats',
-                    )
-                    for section in course
-                ])
-            )
+        sections = list(course_sections)
+        course = select_fields(sections[0],
+            'departmentCode',
+            'courseNumber',
+            'title',
+            'notes',
+            'units',
         )
+        course['sections'] = []
+
+        def filter_fields(section):
+            return select_fields(section,
+                'CRN',
+                'section',
+                'activity',
+                'days',
+                'startTime',
+                'endTime',
+                'instructor',
+                'maxSeats',
+                'takenSeats',
+                'freeSeats',
+            )
+
+        def create_main(section):
+            main = filter_fields(section)
+            main['supplementary'] = []
+            return main
+
+        main = create_main(sections[0])
+        for section in sections[1:]:
+            if section['units'] != 0:
+                course['sections'].append(main)
+                main = create_main(section)
+                continue
+            main['supplementary'].append(filter_fields(section))
+        course['sections'].append(main)
+        courses.append(course)
 
     with open('sample_schedule_courses.json', 'w') as f:
-        json.dump(sections_by_course, f)
+        json.dump(courses, f)
 
 if __name__ == '__main__':
     main()
